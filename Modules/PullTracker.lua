71868f2f5aa7ee77df9f00d8fce92a9d7ccf4bd3
ZelionGG <contact@zeliongg.com>
2026-04-01 00:53:12 +0200

    Disable nameplate event registration for Midnight

--- Modules/PullTracker.lua
+++ Modules/PullTracker.lua
@@ -13,16 +13,16 @@ KeystonePolaris.realPull = {
 
 function KeystonePolaris:InitializePullTracker()
     -- Register events related to pull tracking
-    self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
-    self:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
     self:RegisterEvent("PLAYER_REGEN_ENABLED")
     self:RegisterEvent("ENCOUNTER_END")
-    
+
     if(not self.isMidnight) then
+        self:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+        self:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
         self:RegisterEvent("PLAYER_REGEN_DISABLED")
         self:RegisterEvent("UNIT_THREAT_LIST_UPDATE")
     end
-    
+
     -- Initialize state
     self.realPull.mobs = {}
     self.realPull.sum = 0
