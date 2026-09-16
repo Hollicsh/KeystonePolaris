16eba94edf479a62831d362138ffbf10b0aa91a5
ZelionGG <contact@zeliongg.com>
2026-09-16 10:13:02 +0200

    docs(changelog): add 3.11.5 group reminder notes
    Record the new leader full-group popup.

---
+++ Modules/Changelog/3.11.5.lua
@@ -0,0 +1,83 @@
+local AddOnName, KeystonePolaris = ...;
+
+local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName, true);
+
+KeystonePolaris.Changelog["3.11.5"] = {
+    version_string = "3.11.5",
+    release_date = "2026/09/16",
+    header = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            title = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t Version 3.11.5 - Group Reminder for leaders|r",
+            text = "Version 3.11.5 adds an option to show the [Group Reminder] popup when your Mythic+ group is full and you are the group leader.",
+        },
+        ["frFR"] = {
+            title = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t Version 3.11.5 - Rappel de groupe pour le chef|r",
+            text = "La version 3.11.5 ajoute une option pour afficher la popup de [Rappel de groupe] lorsque votre groupe Mythique+ est complet et que vous en êtes le chef.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "Added an option to show the [Group Reminder] popup when your Mythic+ group is full and you are the group leader.",
+        },
+        ["frFR"] = {
+            "Ajout d'une option pour afficher la popup de [Rappel de groupe] lorsque votre groupe Mythique+ est complet et que vous en êtes le chef.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {},
+        ["deDE"] = {},
+        ["esES"] = {},
+        ["esMX"] = {},
+        ["itIT"] = {},
+        ["ptBR"] = {}
+    }
+}
